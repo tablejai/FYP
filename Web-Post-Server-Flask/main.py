8e835f8b3a2789b6b161cf0d38f58d8732783739
Tablejai <42264922+tablejai@users.noreply.github.com>
2022-10-25 20:36:03 +0800

    Revert "Merge?"
    This reverts commit 0f454a01739e6ceb0ec47bd6512b43411074d89a.

--- Web-Post-Server-Flask/main.py
+++ Web-Post-Server-Flask/main.py
@@ -1,13 +1,11 @@
-from flask import Flask, redirect, url_for, request, jsonify
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 
 @app.route("/hello", methods=['POST'])
 def hello_world():
-    data = request.json
-    print(data)
-    return "Request Received"
+    return "What lun the fuck"
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    app.run(debug=True)
