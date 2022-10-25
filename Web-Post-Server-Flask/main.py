62ecd106c83e79f66edc2cfc671cd157e092f720
Tablejai <alexchan815@gmail.com>
2022-10-25 20:46:59 +0800

    Debug Mode and Host change

--- Web-Post-Server-Flask/main.py
+++ Web-Post-Server-Flask/main.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 
 @app.route("/hello", methods=['POST'])
 def hello_world():
-    return "What lun the fuck"
+    data = request.json
+    print(data)
+    return "Request Received"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False)
