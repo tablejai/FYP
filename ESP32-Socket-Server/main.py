09eb5a34a0beb72fd3999b291acd9531b91ff609
Tablejai <alexchan815@gmail.com>
2022-11-07 22:58:34 +0800

    Socket Server Test

---
+++ ESP32-Socket-Server/main.py
@@ -0,0 +1,27 @@
+import socket
+
+HOST = '0.0.0.0'
+PORT = 5001
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen(5)
+
+print('server start at: %s:%s' % (HOST, PORT))
+print('wait for connection...')
+
+while True:
+    conn, addr = s.accept()
+    print('connected by ' + str(addr))
+
+    while True:
+        indata = conn.recv(1024)
+        if len(indata) == 0: # connection closed
+            conn.close()
+            print('client closed connection.')
+            break
+        print('recv: ' + indata.decode())
+
+        outdata = 'echo ' + indata.decode()
+        conn.send(outdata.encode())
